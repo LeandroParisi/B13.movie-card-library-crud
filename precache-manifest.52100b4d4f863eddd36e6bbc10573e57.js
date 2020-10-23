72807390b2bd24c808194de862cbed1ff8700a92
LeandroParisi <lepcbelisario@gmail.com>
2020-10-22 22:30:25 -0300

    Updates

---
+++ precache-manifest.52100b4d4f863eddd36e6bbc10573e57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2174c5b8b606602dd322bd5db5e78d06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca2e42e5328129705260",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e242323eda1890236c8a",
+    "url": "/static/js/2.d942e3f0.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.d942e3f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca2e42e5328129705260",
+    "url": "/static/js/main.e394f848.chunk.js"
+  },
+  {
+    "revision": "2b256fee03ba42c9a303",
+    "url": "/static/js/runtime-main.1ad417ff.js"
+  }
+]);
